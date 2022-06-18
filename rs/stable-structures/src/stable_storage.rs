ac3ea313f19a40a2422ebb2a11a481d8b0e9be1e
aewc <liquan.eth@gmail.com>
2022-06-18 09:54:00 +0800

    update StableStorage in stable-structures

--- rs/stable-structures/src/stable_storage.rs
+++ rs/stable-structures/src/stable_storage.rs
@@ -1,16 +1,13 @@
 use crate::{Memory, WASM_PAGE_SIZE};
 use ic_cdk::api::stable::{stable64_grow, stable64_read, stable64_size, stable64_write};
-use std::marker::PhantomData;
 
 const GB: u64 = 1 << 30;
 const STABLE_MEMORY_SIZE: u64 = 8 * GB;
 const MAX_PAGES: u64 = STABLE_MEMORY_SIZE / WASM_PAGE_SIZE;
 
 /// A `Memory` that is based on canister stable storage.
-#[derive(Clone)]
-pub struct StableStorage {
-    _marker: PhantomData<()>,
-}
+#[derive(Clone, Default)]
+pub struct StableStorage;
 
 impl Memory for StableStorage {
     fn size(&self) -> u64 {
@@ -54,13 +51,5 @@ impl Memory for StableStorage {
             panic!("write: out of bounds");
         }
         stable64_write(offset, src);
-    }
-}
-
-impl StableStorage {
-    pub fn new() -> Self {
-        Self {
-            _marker: PhantomData,
-        }
     }
 }
